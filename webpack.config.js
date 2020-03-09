e37a6ba8e9ea9042c89729748f4833fb3a8c799d
Tatyana Fomina <tayna-anita@mail.ru>
2020-03-09 23:57:03 +0300

    Промежуточный коммит

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+module.exports = {
+  entry: "./src/index.tsx",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  },
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.min.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "awesome-typescript-loader"
+      }
+    ]
+  }
+};
